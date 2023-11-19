ae2e64b4d979dd0ad437a9f3e126f09b994430a5
elishakaranja <145563815+elishakaranja@users.noreply.github.com>
2023-11-19 15:47:44 +0300

    listed out bots

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import BotCollection from './BotCollection';
+import BotArmy from './BotArmy';
+import BotDetails from './BotDetails';
 import './App.css';
 
 function App() {
+  const [bots, setBots] = useState([]);
+  const [selectedBots, setSelectedBots] = useState([]);
+  const [selectedBot, setSelectedBot] = useState(null);
+
+  useEffect(() => {
+    fetchBots();
+  }, []);
+
+  function fetchBots() {
+    fetch('http://localhost:8001/bots')
+      .then((r) => r.json())
+      .then((bots) => setBots(bots));
+  }
+
+  function handleBotClick(bot) {
+    setSelectedBot(bot);
+    setSelectedBots((prevSelectedBots) => (
+      prevSelectedBots.find((selectedBot) => selectedBot.id === bot.id)
+        ? prevSelectedBots
+        : [...prevSelectedBots, bot]
+    ));
+  }
+
+  function releaseBot(botId) {
+    console.log("Releasing Bot ID:", botId);
+    setSelectedBots((prevSelectedBots) => {
+      const newSelectedBots = prevSelectedBots.filter((bot) => bot.id !== botId);
+      console.log("New Selected Bots:", newSelectedBots);
+      return newSelectedBots;
+    });
+  }
+
+  function handleBotDischarge(botId) {
+    console.log("Discharging Bot ID:", botId);
+    fetch(`http://localhost:8001/bots/${botId}`, {
+      method: 'DELETE',
+    }).then(() => {
+      setSelectedBots((prevSelectedBots) => prevSelectedBots.filter((bot) => bot.id !== botId));
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BotArmy army={selectedBots} onBotClick={handleBotClick} onRelease={releaseBot} onDischarge={handleBotDischarge} />
+      <BotCollection bots={bots} onBotClick={handleBotClick} />
+      <BotDetails selectedBot={selectedBot} />
     </div>
   );
 }
---
+++ src/BotDetails.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function BotDetails({ selectedBot }) {
+  if (!selectedBot) {
+    return <div>No bot selected</div>;
+  }
+
+  return (
+    <div className="bot-details">
+      <h2>Selected Bot Details</h2>
+      <img src={selectedBot.avatar_url} alt={selectedBot.name} />
+      <h3>{selectedBot.name}</h3>
+      <p>Class: {selectedBot.bot_class}</p>
+      <p>Health: {selectedBot.health}</p>
+      <p>Damage: {selectedBot.damage}</p>
+      <p>Armor: {selectedBot.armor}</p>
+      <p>Catchphrase: {selectedBot.catchphrase}</p>
+    </div>
+  );
+}
+
+export default BotDetails;
